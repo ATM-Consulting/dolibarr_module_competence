d0fa76f345add5baa53b2fb384737b7d7b5d9824
florian HENRY <florian.henry@atm-consulting.fr>
2016-08-24 13:39:36 +0200

    better module naming

--- config.default.php
+++ config.default.php
@@ -2,8 +2,9 @@
 
 	if(is_file('../main.inc.php'))$dir = '../';
 	else  if(is_file('../../../main.inc.php'))$dir = '../../../';
+	else  if(is_file('../../../../main.inc.php'))$dir = '../../../../';
+	else  if(is_file('../../../../../main.inc.php'))$dir = '../../../../../';
 	else $dir = '../../';
-
 
 	if(!defined('INC_FROM_DOLIBARR') && defined('INC_FROM_CRON_SCRIPT')) {
 		include($dir."master.inc.php");
@@ -13,16 +14,16 @@
 	} else {
 		global $dolibarr_main_db_host, $dolibarr_main_db_name, $dolibarr_main_db_user, $dolibarr_main_db_pass;
 	}
-
-	if(!defined('DB_HOST')) {
+	if(!defined('DB_HOST') && !empty($dolibarr_main_db_host)) {
 		define('DB_HOST',$dolibarr_main_db_host);
 		define('DB_NAME',$dolibarr_main_db_name);
 		define('DB_USER',$dolibarr_main_db_user);
 		define('DB_PASS',$dolibarr_main_db_pass);
 		define('DB_DRIVER',$dolibarr_main_db_type);
 	}
-	
+
 	dol_include_once('/abricot/inc.core.php');
 
-	
+
+
 	define('DOL_ADMIN_USER', __val($conf->global->RH_DOL_ADMIN_USER, 'admin') );
